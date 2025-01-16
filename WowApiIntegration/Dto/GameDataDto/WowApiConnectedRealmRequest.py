b4d281e878cba4af1aed055b78953d26a97046ee
semutoO <sean.olivieri@gmail.com>
2025-01-15 21:23:03 -0500

    Added abstract searching requests with key/value pairs for the invoker to consume and add to urlParams.

--- WowApiIntegration/Dto/GameDataDto/WowApiConnectedRealmRequest.py
+++ WowApiIntegration/Dto/GameDataDto/WowApiConnectedRealmRequest.py
@@ -1,11 +1,11 @@
-from WowApiIntegration.Dto import AbstractWowApiRequest
+from WowApiIntegration.Dto import AbstractWowApiRequest, AbstractWowApiSearchRequest
 
 
 class WowApiConnectedRealmRequest(AbstractWowApiRequest.AbstractWowApiRequest):
-    def __init__(self, endpoint = None, status = None, realmTimeZone = "America/New_York", orderBy = "id", page = 1, connectedRealmId = None, region="us", namespace="dynamic-us", locale="en_US"):
+    def __init__(self, connectedRealmId = None, endpoint = None, region="us", namespace="dynamic-us", locale="en_US"):
         super().__init__(region, namespace, locale, endpoint)        
-        self.connectedRealmId = connectedRealmId
-        self.status = status
-        self.realmTimeZone = realmTimeZone
-        self.orderBy = orderBy
-        self._page = page
+        self.connectedRealmId = connectedRealmId        
+
+class WowApiConnectedRealmSearchRequest(AbstractWowApiSearchRequest.AbstractWowApiSearchRequest):
+    def __init__(self, orderBy = None, page = 1, pageSize = 50, endpoint = None, region=None, namespace="dynamic-us", locale=None, searchDict:dict = None):
+        super().__init__(orderBy, page, pageSize, endpoint, region, namespace, locale, searchDict)
